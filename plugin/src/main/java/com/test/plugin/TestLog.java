aca6717caf810ae671b1085b0ac3008f81d681bb
unknown <liuzhaopan@360buyad.local>
2018-05-20 09:21:51 +0800

    android plugin demo for debug anroid gradle plugin source code

---
+++ plugin/src/main/java/com/test/plugin/TestLog.java
@@ -0,0 +1,7 @@
+package com.test.plugin;
+
+public class TestLog {
+    public static void log(String tag, String message) {
+        System.out.println("############### " + tag + " : " + message + " ################");
+    }
+}
---
+++ plugin/src/main/java/com/test/plugin/TestPlugin.java
@@ -0,0 +1,11 @@
+package com.test.plugin;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class TestPlugin implements Plugin<Project> {
+    @Override
+    public void apply(Project project) {
+
+    }
+}
